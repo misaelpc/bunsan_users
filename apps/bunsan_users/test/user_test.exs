57f3ef6ab8a2f7b9421dc27700616054ba56a9c9
Misael Pérez Chamorro <misaelperezchamorro@Misaels-MacBook-Pro.local>
2018-06-04 10:31:47 -0500

    Bind with phoenix

---
+++ apps/bunsan_users/test/user_test.exs
@@ -0,0 +1,33 @@
+defmodule BunsanUsers.UserTest do
+
+  use BunsanUsers.DataCase
+
+  setup do
+    user = %BunsanUsers.User{}
+    user_chg = BunsanUsers.User.signup_changeset(user, %{"name" => "Julio", "age" => 33, "email" => "julio@julio.com"})
+    stored_user = BunsanUsers.Repo.insert!(user_chg)
+    {:ok, user: stored_user}
+  end
+
+  test "create user succesfully" do
+    user = %BunsanUsers.User{}
+    changeset = BunsanUsers.User.signup_changeset(user, %{"name" => "Misael", "age" => 33, "email" => "misa@misa.com"})
+    assert changeset.valid? == true
+  end
+
+  test "create user unsuccesfully" do
+    user = %BunsanUsers.User{}
+    changeset = BunsanUsers.User.signup_changeset(user, %{"name" => "Misael", "age" => "A", "email" => "misa@misa.com"})
+    assert changeset.valid? == false
+  end
+
+  test "search user succesfully", %{user: user} do
+    user = BunsanUsers.User.search(user.id)
+    assert user.name == "Julio"
+  end
+
+  test "search all users" do
+    users = BunsanUsers.User.search_all
+    assert users != []
+  end
+end
--- apps/bunsan_users_web/lib/bunsan_users_web/controllers/user_controller.ex
+++ apps/bunsan_users_web/lib/bunsan_users_web/controllers/user_controller.ex
@@ -3,11 +3,21 @@ defmodule BunsanUsersWeb.UserController do
   Controller for Users
   """
   use BunsanUsersWeb, :controller
+  alias BunsanUsers.User
 
-  def create(conn, _params) do
-    conn
-      |> put_status(201)
-      |> json(%{user: "user"})
+  def create(conn, %{"user" => user_params}) do
+    chg = User.signup_changeset(%User{}, user_params)
+    case chg.valid? do
+      true ->
+        {:ok, user} = User.save(chg)
+        conn
+          |> put_status(201)
+          |> json(%{user: %{name: user.name, age: user.age, email: user.email}}) #View
+      false ->
+        conn
+          |> put_status(400)
+          |> json(%{error: "Unable to create user"})
+    end
   end
 
   def index(conn, _params) do
